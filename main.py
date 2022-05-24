976bff44a4a92394ce800fe56f71775efe6e972a
Mot0511 <suvorov.matvej9@gmail.com>
2022-05-24 21:39:44 +0300

    _

---
+++ main.py
@@ -0,0 +1,34 @@
+import hashlib
+import os
+
+sender = input('Отправитель: ')
+money = input('Кол-во: ')
+to = input('Получатель: ')
+
+filesStr = os.listdir('blockchain')
+files = []
+
+hash = hashlib.sha256()
+if (filesStr == []):
+    hash.update(bytes('0' + sender + money + to, encoding='utf8'))
+    hash = hash.hexdigest()
+
+    block = open(f'blockchain/1', 'w')
+    block.write(hash + '\n' + '0' + '\n' + sender + '\n' + money + '\n' + to)
+
+else:
+    for i in filesStr:
+        files.append(int(i))
+
+    maxFile = max(files)
+
+    prevHash = open(f'blockchain/{maxFile}', 'r')
+    prevHashText = []
+    for i in prevHash:
+        prevHashText.append(i.strip())
+
+    hash.update(bytes(prevHashText[0] + sender + money + to, encoding='utf8'))
+    hash = hash.hexdigest()
+
+    block = open(f'blockchain/{str(maxFile + 1)}', 'w')
+    block.write(hash + '\n' + prevHashText[0] + '\n' + sender + '\n' + money + '\n' + to)
