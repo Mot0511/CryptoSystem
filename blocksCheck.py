2b489a20134a6e072e193fe39285896c05b39ed4
Mot0511 <suvorov.matvej9@gmail.com>
2022-05-25 21:23:35 +0300

    _

--- blocksCheck.py
+++ blocksCheck.py
@@ -1,6 +1,23 @@
 import os
 import readLines
+import hashlib
 
-numBlocks = len(os.listdir('blockchain'))
-for i in range(0, numBlocks):
-    readLines.lines('blockchain/' + str(i))
+run = True
+while True:
+    numBlocks = len(os.listdir('blockchain'))
+    for i in range(1, numBlocks):
+        lines1 = readLines.lines('blockchain/' + str(i))
+        lines2 = readLines.lines('blockchain/' + str(i + 1))
+
+        hash = hashlib.sha256()
+        hash.update(bytes(lines1[1] + lines1[2] + lines1[2] + lines1[4], encoding='utf8'))
+        hash = hash.hexdigest()
+
+        if (not(hash == lines2[1])):
+            run = False
+            break
+
+    if (run == False):
+        break
+
+print('!!!!!!!ERROR!!!!!!!!!')
--- readLines.py
+++ readLines.py
@@ -1,5 +1,5 @@
-rlines = []
 def lines(file):
+    rlines = []
     f = open(file, 'r')
     for i in f:
         rlines.append(i.strip())
